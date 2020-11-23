7af9000384423a971f6eb2046d8ba8d824ea7d3d
Inigo Jiron <pohatu678@msn.com>
2020-11-23 04:13:52 -0500

    Working commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,83 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <ul v-for="(t, idx) in timers" v-bind:key="makeKey(idx, t.timestamp)">
+        <Timer
+          :id="idx"
+          :name="t.name"
+          :timestamp="t.timestamp"
+          :length="t.length"
+          :updateFunction="updateTimer"
+          :deleteFunction="deleteTimer"
+        ></Timer>
+      </ul>
+    </div>
+    <div>
+      <button class="mainButton" v-on:click="addTimer">Add Timer</button>
+    </div>
+    <div>
+      <button class="mainButton" v-on:click="resetTimers">Delete All</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from "vue";
+import Timer from "./components/Timer.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Timer,
+  },
+  data: function () {
+    return { timers: [] };
+  },
+  methods: {
+    makeKey(index, timestamp) {
+      return index + ":" + timestamp;
+    },
+    loadTimers: function () {
+      if (localStorage.timers) {
+        this.timers = JSON.parse(localStorage.timers);
+      }
+    },
+    saveTimers: function () {
+      localStorage.setItem("timers", JSON.stringify(this.timers));
+    },
+    deleteTimer(idx) {
+      this.timers.splice(idx, 1);
+      this.saveTimers();
+    },
+    updateTimer(timer) {
+      let idx = timer.id;
+      Vue.delete(timer, "id");
+      this.timers.splice(idx, 1, timer);
+      this.saveTimers();
+    },
+    makeTimer(name, timestamp, length) {
+      return {
+        name: name,
+        timestamp: timestamp,
+        length: length,
+      };
+    },
+    makeDefaultTimer() {
+      return this.makeTimer("Timer Name", -1, 24);
+    },
+    addTimer() {
+      this.timers.push(this.makeDefaultTimer());
+      this.saveTimers();
+    },
+    resetTimers() {
+      this.timers = [];
+      this.saveTimers();
+    },
+  },
+  mounted: function () {
+    this.loadTimers();
+  },
+};
 </script>
 
 <style>
@@ -21,8 +85,41 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 1em;
+  color: var(--text-color);
+
+  --corners: 25px;
+  --content-width: 50%;
+  --main-button-height: 1.5em;
+  --main-button-font-size: 2em;
+  --bg-color: rgb(60, 60, 60);
+  --text-color: white;
+}
+html {
+  background: rgb(80, 80, 80);
+}
+ul {
+  display: inline;
+  justify-content: center;
+}
+.timer {
+  width: var(--content-width);
+}
+input {
+  color: var(--text-color);
+  background: var(--bg-color);
+}
+button {
+  color: var(--text-color);
+  background: var(--bg-color);
+}
+.mainButton {
+  margin-top: 1em;
+  height: var(--main-button-height);
+  font-size: var(--main-button-font-size);
+  width: var(--content-width);
+  border-radius: var(--corners);
 }
 </style>
---
+++ src/components/Timer.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="timer">
+    <input class="text" v-model="fieldName" />
+    <h1 v-bind:style="timeStyle">{{ remainingTimeToString(remainingTime) }}</h1>
+    <div class="radio-group" v-for="o in options" v-bind:key="o">
+      <div class="radio-option">
+        <label :for="o">{{ o + "H" }}</label>
+        <input class="radio" type="radio" :value="o" v-model="picked" />
+      </div>
+    </div>
+    <button class="setButton" v-on:click="this.updateTimestamp">
+      Set Timer
+    </button>
+    <div class="smallButtons">
+      <button class="stopButton" v-on:click="stopTimer">Stop</button>
+      <button class="deleteButton" v-on:click="deleteFunction(id)">
+        Delete
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Timer",
+  props: {
+    id: Number,
+    name: String,
+    timestamp: Number,
+    length: Number,
+    updateFunction: Function,
+    deleteFunction: Function,
+  },
+  data: function () {
+    return {
+      picked: this.length,
+      options: [12, 24, 48, 72],
+      remainingTime: Math.max(0, this.endTime() - Date.now()),
+    };
+  },
+  methods: {
+    update(id, name, timestamp, length) {
+      this.updateFunction({
+        id: id,
+        name: name,
+        timestamp: timestamp,
+        length: length,
+      });
+    },
+    updateLength() {
+      this.update(this.id, this.name, this.timestamp, this.picked);
+    },
+    updateTimestamp() {
+      this.update(this.id, this.name, Date.now(), this.picked);
+    },
+    endTime() {
+      return this.timestamp + this.length * 60 * 60 * 1000;
+    },
+    stopTimer() {
+      this.update(this.id, this.name, -1, this.picked);
+    },
+    remainingTimeToString(milliseconds) {
+      // https://stackoverflow.com/questions/19700283/how-to-convert-time-milliseconds-to-hours-min-sec-format-in-javascript
+      // Get hours from milliseconds
+      var hours = milliseconds / (1000 * 60 * 60);
+      var absoluteHours = Math.floor(hours);
+      var h = absoluteHours > 9 ? absoluteHours : "0" + absoluteHours;
+
+      // Get remainder from hours and convert to minutes
+      var minutes = (hours - absoluteHours) * 60;
+      var absoluteMinutes = Math.floor(minutes);
+      var m = absoluteMinutes > 9 ? absoluteMinutes : "0" + absoluteMinutes;
+
+      // Get remainder from minutes and convert to seconds
+      var seconds = (minutes - absoluteMinutes) * 60;
+      var absoluteSeconds = Math.floor(seconds);
+      var s = absoluteSeconds > 9 ? absoluteSeconds : "0" + absoluteSeconds;
+
+      return h + ":" + m + ":" + s;
+    },
+  },
+  computed: {
+    fieldName: {
+      get() {
+        return this.name;
+      },
+      set(newValue) {
+        this.update(this.id, newValue, this.timestamp, this.length);
+      },
+    },
+    timeStyle() {
+      if (this.remainingTime > 0) {
+        return { color: "red" };
+      }
+      return { color: "green" };
+    },
+  },
+  watch: {
+    remainingTime: {
+      handler(value) {
+        if (value > 0) {
+          setTimeout(() => {
+            this.remainingTime = Math.max(0, this.endTime() - Date.now());
+          }, 1000);
+        }
+      },
+      immediate: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.timer {
+  margin: 0 auto;
+  border: 2px solid black;
+  border-radius: var(--corners);
+
+  --corners: 25px;
+}
+input.text {
+  width: 100%;
+  height: 1.5em;
+  font-size: 2em;
+  outline-width: 0;
+  box-sizing: border-box;
+  text-align: center;
+  border-top-left-radius: var(--corners);
+  border-top-right-radius: var(--corners);
+  box-sizing: border-box;
+}
+h1 {
+  text-align: center;
+  font-size: 3em;
+  margin: 0;
+}
+.smallButtons {
+  text-align: right;
+}
+.stopButton,
+.deleteButton {
+  width: 25%;
+}
+.deleteButton {
+  border-bottom-right-radius: var(--corners);
+}
+.setButton {
+  width: 100%;
+  height: 1.5em;
+  font-size: 2.5em;
+}
+.radio-group {
+  display: inline-block;
+  margin-left: 3px;
+  margin-right: 3px;
+}
+.radio {
+  display: block;
+  width: 2em;
+  height: 2em;
+  margin-bottom: 5px;
+}
+</style>
