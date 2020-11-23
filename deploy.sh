8d4cba986b1df49817ab3f4fe6942318e3be1eaf
Inigo Jiron <pohatu678@msn.com>
2020-11-23 05:05:58 -0500

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,15 @@
 #!/usr/bin/env sh
 
-# abort on errors
 set -e
 
-# build
 npm run build
 
-# navigate into the build output directory
 cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
+git remote add origin https://github.com/Barkuto/genshin_timer.git
+git checkout --orphan gh-pages
 git add -A
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:barkuto/genshin_timer.git main:gh-pages
+git push --set-upstream origin gh-pages
 
 cd -
