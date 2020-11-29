5c95e6fbd9940875a28a255f7c39b2d5e6bdf865
Inigo Jiron <pohatu678@msn.com>
2020-11-29 18:54:08 -0500

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git remote add origin https://github.com/Barkuto/genshin_timer.git
 git checkout --orphan gh-pages
 git add -A
 git commit -m 'deploy'
-git push --set-upstream origin gh-pages
+git push -f --set-upstream origin gh-pages
 
 cd -
